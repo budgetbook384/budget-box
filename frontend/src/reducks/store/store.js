50167d375082b796e0e9721a325f7ffae05ac93e
Sana <smkhan_27@hotmail.com>
2021-12-05 23:55:46 -0800

    working on front-end

--- frontend/src/reducks/store/store.js
+++ frontend/src/reducks/store/store.js
@@ -25,3 +25,30 @@ export default function createStore(history) {
        )
     )
 }
+
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { applyMiddleware, combineReducers, compose, createStore as reduxCreateStore } from 'redux';
+import thunk from 'redux-thunk';
+
+import { CategoriesReducer } from '../category/reducers';
+import { TransactionsReducer } from '../transactions/reducers';
+import { UserReducer } from '../users/reducers';
+
+export default function createStore(history) {
+    return reduxCreateStore(
+        combineReducers({
+            router: connectRouter(history),
+            user: UserReducer,
+            transactions: TransactionsReducer,
+            categories: CategoriesReducer,
+       }),
+       compose(
+        applyMiddleware(
+            routerMiddleware(history),
+            thunk
+        ),
+        // DEBUG MODE
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+       )
+    );
+}
